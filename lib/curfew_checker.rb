f57f4963d6b192ce7a25c65c9e277ea253d0fd03
ziesop20 <sincerelysophie@hotmail.com>
2018-07-25 13:51:10 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,13 +1,25 @@
 def simple_curfew_checker(time)
-  # code goes here
-end
+  if time >= 11 
+    puts "Curfew"
+  end
+end 
+
+simple_curfew_checker(10)
 
 def curfew_checker(time)
-  # code goes here
+   if time > 23
+    puts "you're in trouble"
+  elsifif time == 23 
+    puts "better get home quick"
+  elsif time < 23 
+    puts "keep having fun"
+  end
 end
 
+curfew_checker(10)
+
 def complex_curfew_checker(time)
-  # code goes here
+  
 end
 
 def deluxe_curfew_checker(time)
